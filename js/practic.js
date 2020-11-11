091615d4f79320394a332963815e40e81e2a7862
Serhii Chyrko <64086212+Segijua06@users.noreply.github.com>
2020-11-11 11:01:15 +0200

    //

--- js/practic.js
+++ js/practic.js
@@ -1,27 +1,55 @@
-const Hotel = function(name, stars, capacity) {
-    this.name = name;
-    this.stars = stars;
-    this.capacity = capacity;
-    this.guestCount = 0;
+// const Hotel = function(name, stars, capacity) {
+//     this.name = name;
+//     this.stars = stars;
+//     this.capacity = capacity;
+//     this.guestCount = 0;
   
-    this.greet = function(guestName) {
-      console.log(`Hello ${guestName}, wellcome to ${this.name}`);
-    };
+//     this.greet = function(guestName) {
+//       console.log(`Hello ${guestName}, wellcome to ${this.name}`);
+//     };
   
-    this.addGuests = function(amount) {
-      this.guestCount += amount;
-    };
+//     this.addGuests = function(amount) {
+//       this.guestCount += amount;
+//     };
   
-    this.removeGuests = function(amount) {
-      this.guestCount -= amount;
-    };
-  };
+//     this.removeGuests = function(amount) {
+//       this.guestCount -= amount;
+//     };
+//   };
   
-  const hotel = new Hotel('Sunrise Hotel', 5, 100);
+//   const hotel = new Hotel('Sunrise Hotel', 5, 100);
   
-  console.log(hotel);
-  // Hotel {name: "Sunrise Hotel", greet: ƒ, addGuests: f, removeGuests: f}
-  hotel.greet('Mango'); // Hello Mango, wellcome to Sunrise Hotel
-  hotel.addGuests(50);
-  hotel.removeGuests(50);
+//   console.log(hotel);
+//   // Hotel {name: "Sunrise Hotel", greet: ƒ, addGuests: f, removeGuests: f}
+//   hotel.greet('Mango'); // Hello Mango, wellcome to Sunrise Hotel
+//   hotel.addGuests(50);
+//   hotel.removeGuests(50);
+
+
+
+const Hero = function(name, xp) {
+  this.name = name;
+  this.xp = xp;
+};
+
+Hero.prototype.gainXp = function(amount) {
+  console.log(`${this.name} gained ${amount} experience points`);
+  this.xp += amount;
+};
+
+const Warrior = function(name, xp, weapon) {
+  Hero.call(this, name, xp);
+
+  this.weapon = weapon;
+};
+
+Warrior.prototype = Object.create(Hero.prototype);
+Warrior.prototype.constructor = Warrior;
+
+Warrior.prototype.attack = function() {
+  console.log(`${this.name} attacks with ${this.weapon}`);
+};
+
+const poly = new Warrior('Poly', 200, 'sword');
+
   
--- js/task-1.js
+++ js/task-1.js
@@ -9,7 +9,14 @@
 
 
  // Write code under this line
+    const Account = function(login, email) {
+        this.login = login;
+        this.email = email;
+    };
     
+    Account.prototype.getInfo = function () {
+        return `login : ${this.login}, email: ${this.email}`;
+        }
 
  console.log(typeof Account.prototype.getInfo);
  // 'function'
